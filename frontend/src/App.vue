d0886a2e2f2aa1d1e535431e2594a37a580ba6dc
Arc <arclight04@gmail.com>
2024-05-22 13:34:56 +0800

    add dummy content

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,29 @@
 
 <template>
-	<div class="flex">
-		<Sidebar />
-		<div class="flex-1">
-			<!-- <div>Hello world</div>
-			<button class="bg-primary">Toggle Modes</button> -->
+	<div class="flex min-h-screen">
+		<Sidebar :categories="categories" @categorySelected="updateCategoryName"/>
+		<div class="flex-1 px-4 py-20 md:px-20 ">
+			<div class="flex justify-between">
+				<h1 class="text-4xl font-bold font-heading">{{ selectedCategoryName }}</h1>
+				<button class="bg-primary rounded-md py-2.5 px-4" @click="addTask">Add Task</button>
+			</div>
+			<div class="flex flex-col gap-2 mt-8 text-text-default">
+				<div
+					v-for="task in selectedCategoryTasks"
+					:key="task.content"
+					class="bg-primary-200 flex gap-4 items-center rounded-md shadow px-5 py-2.5" >
+					<input type="checkbox" :checked="task.done">
+					<p>
+						{{ task.content }}
+					</p>
+				</div>
+			</div>
 		</div>
 	</div>
 </template>
 
 <script>
+import { computed, reactive, ref } from 'vue';
 import Sidebar from './components/Sidebar.vue';
 
 export default {
@@ -18,14 +32,67 @@ export default {
 		Sidebar,
 	},
 	setup() {
-		return {
-			
+		const selectedCategoryName = ref('Home')
+		const categories = reactive(
+			[
+				{
+					categoryName: "Home",
+					data: []
+				},
+				{ 
+					categoryName: "OJT",
+					data: [
+						{
+						content: "Finish Vue Project",
+						done: false,
+						},
+						{
+						content: "Admin and User model separation",
+						done: true,
+						},	
+					],
+				},
+				{ 
+				categoryName: "WriteUps",
+				data: [
+					{
+					content: "Settings Layout",
+					done: false,
+					},
+					{
+					content: "List of blocked users",
+					done: false,
+					},	
+				],
+				}
+			]
+		)
+
+		const updateCategoryName = (categoryName) => {
+			selectedCategoryName.value = categoryName
 		}
-	}
+
+		const selectedCategoryTasks = computed(() => {
+			const category = categories.find(category => category.categoryName == selectedCategoryName.value)
+			return category ? category.data : []
+		})
+		
+		const addTask = () => {
+			const category = categories.find(category => category.categoryName === selectedCategoryName.value)
+			category.data.push({
+				content: 'New Content',
+				done: false,
+			})
+		}
+
+		return {
+			categories,
+			selectedCategoryName,
+			updateCategoryName,
+			selectedCategoryName,
+			selectedCategoryTasks,
+			addTask,
+		}
+	},
 }
-</script>
-
-<style scoped>
-
-</style>
-
+</script>	
--- frontend/src/components/Sidebar.vue
+++ frontend/src/components/Sidebar.vue
@@ -1,24 +1,17 @@
 <template>
-    <div class="flex flex-col font-body h-screen border-r border-slate-800 max-w-[280px] w-full px-8 py-10">
+    <div class="hidden md:flex flex-col font-body h-screen border-r border-slate-800 max-w-[280px] w-full px-8 py-10">
         <div class="flex items-center justify-between">
-            <h1 class="text-2xl font-bold font-heading">Categories</h1>
+            <h1 class="text-lg font-bold font-heading text-primary">Categories</h1>
         </div>
         <div class="flex flex-col gap-2 px-2 mt-8 overflow-y-auto text-sm max-h-[500px]">
-            <div class="px-2 py-2 rounded hover:border-primary hover:cursor-pointer">Category 1</div>
-            <div class="px-2 py-2 rounded hover:border-primary hover:cursor-pointer">Category 2</div>
-            <div class="px-2 py-2 rounded hover:border-primary hover:cursor-pointer">Category 3</div>
-            <div class="px-2 py-2 rounded hover:border-primary hover:cursor-pointer">Category 3</div>
-            <div class="px-2 py-2 rounded hover:border-primary hover:cursor-pointer">Category 3</div>
-            <div class="px-2 py-2 rounded hover:border-primary hover:cursor-pointer">Category 3</div>
-            <div class="px-2 py-2 rounded hover:border-primary hover:cursor-pointer">Category 3</div>
-            <div class="px-2 py-2 rounded hover:border-primary hover:cursor-pointer">Category 3</div>
-            <div class="px-2 py-2 rounded hover:border-primary hover:cursor-pointer">Category 3</div>
-            <div class="px-2 py-2 rounded hover:border-primary hover:cursor-pointer">Category 3</div>
-            <div class="px-2 py-2 rounded hover:border-primary hover:cursor-pointer">Category 3</div>
-            <div class="px-2 py-2 rounded hover:border-primary hover:cursor-pointer">Category 3</div>
-            <div class="px-2 py-2 rounded hover:border-primary hover:cursor-pointer">Category 3</div>
-            <div class="px-2 py-2 rounded hover:border-primary hover:cursor-pointer">Category 3</div>
-            <div class="px-2 py-2 rounded hover:border-primary hover:cursor-pointer">Category 3</div>
+            <div
+                v-for="category in categories"
+                :key="category.categoryName"
+                @click="handleClick(category.categoryName)"
+                class="px-2 py-2 rounded hover:border-primary hover:cursor-pointer hover:bg-primary dark:hover:bg-secondary dark:hover:text-white"
+            >
+                {{ category.categoryName }}
+            </div>
         </div>
         <fwb-toggle v-model="isDark" label="Toggle Dark Mode" class="mt-auto"/>
     </div>
@@ -29,22 +22,30 @@ import { FwbToggle } from 'flowbite-vue';
 import { useDark, useToggle } from '@vueuse/core';
 
 export default {
+    
     name: 'Sidebar',
     components: {
         FwbToggle,
     },
-    setup() {
+    props: {
+        categories: {
+            type: Array,
+            required: true,
+        },
+    },
+    setup(props, { emit }) {
         const isDark = useDark()
         const toggleDark = useToggle(useDark)
+
+        const handleClick = (categoryName) => {
+            emit('categorySelected', categoryName)
+        }
 
         return {
             isDark,
             toggleDark,
+            handleClick
         }
     }
 }
 </script>
-
-<style>
-
-</style>
