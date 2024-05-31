620e0b3bb4e0952a88c6c44734e32cac48a8fb8a
Arc <arclight04@gmail.com>
2024-05-31 16:08:11 +0800

    implemented router setup

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,7 +6,11 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { faPlus, faTrash } from '@fortawesome/free-solid-svg-icons'
 import App from './App.vue'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import router from './router'
 
 library.add(faTrash, faPlus)
 
-createApp(App).component("font-awesome-icon", FontAwesomeIcon).mount('#app')
+const app = createApp(App)
+app.use(router)
+app.component("font-awesome-icon", FontAwesomeIcon)
+app.mount('#app')
